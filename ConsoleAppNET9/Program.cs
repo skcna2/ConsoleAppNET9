a7568629ecba42a0b6f5c7b1e349a4a11dc5dd66
skcna2 <skcna2@gmail.com>
2025-03-25 15:31:19 +0100

    primerswitch

--- ConsoleAppNET9/Program.cs
+++ ConsoleAppNET9/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace ConsoleAppNET9
+﻿using System.ComponentModel;
+
+Console.WriteLine("Please type your name: ");
+
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("------------------------------------------");
+Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself\n");
+Console.WriteLine(@$"What game would you like to play today? Choose from the options below: 
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program"); // @$  - Para escribir en varias líneas
+Console.WriteLine("-------------------------------------------");
+
+var gameSelected = Console.ReadLine();
+
+
+switch (gameSelected.Trim().ToLower())
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    case "a":
+        Console.WriteLine("Addition game selected");
+        break;
+    case "s":
+        Console.WriteLine("Substraction game selected");
+        break;
+    case "m":
+        Console.WriteLine("Multiplication game selected");
+        break;
+    case "d":
+        Console.WriteLine("Dicision game selected");
+        break;
+    case "q":
+        Console.WriteLine("Goodbye");
+        break;
+    default:
+        Console.WriteLine("Opción no válida");
+        break;
+
 }
+
